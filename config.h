a0960312cb1b181b543e95bb92698571d5c01733
echomachst8745 <joel.crann22@hotmail.co.uk>
2026-08-01 02:05:27 +0100

    Added vpn region

--- config.h
+++ config.h
@@ -72,7 +72,7 @@ static const struct arg args[] = {
 	{ ram_perc,     "[\uefc5  %3s%%]",      NULL              },
 	{ print,        "%s",                   " | "             },
 	{ run_command,  "[\U000f05a9 %3s%%]",   "nmcli -f IN-USE,SIGNAL dev wifi | awk '/\\*/{print $2}'" },
-	{ run_command,  "[VPN %s ]",                   "[ $(piactl get connectionstate) = \"Connected\" ] && echo \"\uf023\" || echo \"\uf2fc\"" },
+	{ run_command,  "[VPN %s ]",                   "[ $(piactl get connectionstate) = \"Connected\" ] && echo \"\uf023 $(piactl get region)\" || echo \"\uf2fc\"" },
 	{ print,        "%s",                   " | "             },
   { run_command,  "[%s  ",                 "wpctl get-volume @DEFAULT_SINK@ | grep -q 'MUTED' && echo \"\ueee8\" || echo \"\uf028\"" },
   { run_command,  "%4s]",                 "pactl get-sink-volume @DEFAULT_SINK@ | grep -oP '\\d+%' | head -1" },
